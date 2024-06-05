1e65d06e3f27630ac1c3e93a68cd1cbc7145b17f
snorlax_codes <siarpit119@gmail.com>
2024-06-05 09:33:58 +0530

    Added multiplication function

--- nole.c
+++ nole.c
@@ -3,9 +3,12 @@
 
 float add(float a, float b);
 float sub(float a, float b);
+float multi(float a, float b);
+
 
 
 int main(){
+    float x, y;
     printf("Hello world I am Nole. I am a real basic chatbot\n");
     printf("I can add, subtract, print table and do many other diffrent kinds of basic maths calculation\n");
     printf("I am still on a building stage. \n");
@@ -35,6 +38,13 @@ int main(){
         scanf("%f", &y);
         sub(x, y);
     }
+    else if(strcmp(user_input, "multi") == 0 || strcmp(user_input, "mulitply") == 0){
+        printf("Enter the first value: ");
+        scanf("%f", &x);
+        printf("Enter the second value: ");
+        scanf("%f", &y);
+        multi(x, y);
+    }
     
     else{
         printf("Invaild input");
@@ -51,4 +61,9 @@ float add(float a, float b){
 float sub(float a, float b){
     float ans2 = a-b;
     printf("subtraction of %f - %f = %f", a, b, ans2);
+}
+
+float multi(float a, float b){
+    float ans3 = a*b;
+    printf("mulitipilcation of %f x %f = %f", a, b, ans3);
 }

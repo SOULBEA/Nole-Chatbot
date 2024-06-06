e689034d580ef2427fc980ffa7dbfafc01fd9373
snorlax_codes <siarpit119@gmail.com>
2024-06-06 23:39:23 +0530

    added area calculator

--- nole.c
+++ nole.c
@@ -1,69 +1,144 @@
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 
-float add(float a, float b);
-float sub(float a, float b);
-float multi(float a, float b);
-
+float add(float a);
+float sub(float a);
+float multi(float a);
+float divide(float a);
+float area_circle(float radius);
+float area_rec(float len, float width);
+float area_tri(float height, float base);
 
 
 int main(){
-    float x, y;
-    printf("Hello world I am Nole. I am a real basic chatbot\n");
-    printf("I can add, subtract, print table and do many other diffrent kinds of basic maths calculation\n");
-    printf("I am still on a building stage. \n");
-
+    
+    printf("============== Hello I am Nole. ==============\n");
+    printf("I can add, subtract, multiply, or divide there are more feature will be coming up very soon\n");
+    printf("Anyway How can I help you today: ");
     char user_input[100];
-    printf("Please enter an instruction : ");
-    scanf("%s", &user_input);
-    
-    if(strcmp(user_input, "hi") == 0 || strcmp(user_input, "hello") == 0){
-        printf("Namaste, How are you doing today");
+    scanf("%s", user_input);
+    if(strcmp(user_input, "Hi") == 0 || strcmp(user_input, "hi") == 0 || strcmp(user_input, "hello") == 0 || strcmp(user_input, "Hello") == 0){
+        printf("Namaste, How are you doing today.");
     }
 
-    else if(strcmp(user_input,"add") == 0 || strcmp(user_input,"addition") == 0){
-        float x, y;
-        printf("Enter first value:  ");
-        scanf("%f", &x);
-        printf("Enter second value to add: ");
-        scanf("%f", &y);
-        add(x, y);
+    else if(strcmp(user_input, "add") == 0 || strcmp(user_input, "addtion") == 0){
+        float x;
+        add(x);
     }
-    else if (strcmp(user_input, "sub") == 0 || strcmp(user_input, "subtraction") == 0)      
+
+    else if(strcmp(user_input, "sub") == 0 || strcmp(user_input, "subtraction") == 0){
+        float x;
+        sub(x);
+    }
+    else if(strcmp(user_input, "multiplication") == 0 || strcmp(user_input, "multiply") == 0){
+        float x;
+        multi(x);
+    }
+    else if(strcmp(user_input, "divide") == 0 || strcmp(user_input, "division") == 0){
+        float x;
+        divide(x);
+    }
+    else if (strcmp(user_input, "circle") == 0|| strcmp(user_input, "area_of_cirlce") == 0)
     {
-        float x, y;
-        printf("Enter first value:  ");
-        scanf("%f", &x);
-        printf("Enter second value to add: ");
-        scanf("%f", &y);
-        sub(x, y);
+        float radius1;
+        printf("Please enter the value of radius: ");
+        scanf("%f", &radius1);
+        area_circle(radius1);
     }
-    else if(strcmp(user_input, "multi") == 0 || strcmp(user_input, "mulitply") == 0){
-        printf("Enter the first value: ");
-        scanf("%f", &x);
-        printf("Enter the second value: ");
-        scanf("%f", &y);
-        multi(x, y);
+    else if (strcmp(user_input, "rectangle") == 0 || strcmp(user_input, "area_of_rectangle") == 0 || strcmp(user_input, "rec") ==  0)
+    {
+        float len, width;
+        area_rec(len, width);
+    }
+
+    else if(strcmp(user_input, "tri") == 0 || strcmp(user_input, "area_of_triangle") == 0){
+        float height, base;
+        area_tri(height, base);
     }
     
-    else{
-        printf("Invaild input");
-    }
+    
     return 0;
 }
 
-float add(float a, float b){
-    float ans1 = a+b;
-    printf("addtion of %f + %f = %f", a, b, ans1);
-    return ans1;
+float add(float a){
+    int n;
+    float ans1 = 0;
+    printf("please enter the total number you want to add: ");
+    scanf("%d", &n);
+    float arr[n];
+    for (int i = 0; i<n; i++){
+        printf("Please enter %d. number: ", i+1);
+        scanf("%f", &arr[i]);
+        ans1 = ans1 + arr[i];
+    }
+    // float ans1 = a+b;
+    printf("Answer = %f", ans1);
 }
 
-float sub(float a, float b){
-    float ans2 = a-b;
-    printf("subtraction of %f - %f = %f", a, b, ans2);
+float sub(float a){
+    int num;
+    float ans2 = 0;
+    printf("please enter the total number you want to subract: ");
+    scanf("%d", &num);
+    float arr[num];
+    for(int i = 0; i<num; i++){
+        printf("Please enter the %d. number: ", i+1);
+        scanf("%f", &arr[i]);
+        ans2 -= arr[i];
+
+    }
+    printf("Answer = %f", ans2);
+    // float ans2 = a-b;
+    // printf("subtraction of %f - %f = %f", a, b, ans2);
+
 }
 
-float multi(float a, float b){
-    float ans3 = a*b;
-    printf("mulitipilcation of %f x %f = %f", a, b, ans3);
+float multi(float a){
+    int num;
+    float ans3 = 1;
+    printf("please enter the total number you want to mulitiply: ");
+    scanf("%d", &num);
+    float arr[num];
+    for(int i = 0;i<num;i++){
+        printf("Please enter %d. number: ", i+1);
+        scanf("%f", &arr[i]);
+        ans3 *= arr[i];
+
+    }
+    printf("Answer = %f", ans3);
+}
+
+float divide(float a){
+    int num;
+    float ans4 = 0;
+    printf("please enter the total number you want to divide: ");
+    scanf("%d", &num);
+    float arr[num];
+    for(int i = 0;i<num; i++){
+        printf("please enter the %d. number: ", i+1);
+        scanf("%f", &arr[i]);
+        float ans4 = ans4/arr[i];
+    }
+    printf("Answer = %f", ans4);
+}
+
+float area_circle(float radius){
+    float ans5 = 3.1415*radius*radius;
+    printf("area of circle = %f", ans5);
+}
+
+float area_rec(float len, float width){
+    printf("Please enter the lenth of the rectangle : ");
+    scanf("%f", &len);
+    printf("Please enter the width of the rectangle : ");
+    scanf("%f", &width);
+    float ans6 = len*width;
+    printf("area of rectangle = %f", ans6);
+}
+
+float area_tri(float height, float base){
+    float ans7 = 0.5*height*base;
+    printf("area of triangle = %f", ans7);
+
 }

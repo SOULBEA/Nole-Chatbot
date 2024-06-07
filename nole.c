35cd76b99e6fa6ac4383fc3177c8d20896b69006
snorlax_codes <siarpit119@gmail.com>
2024-06-07 14:36:00 +0530

    adding quiz function which is not working

--- nole.c
+++ nole.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
 #include<math.h>
 
 float add(float a);
@@ -7,9 +10,116 @@ float sub(float a);
 float multi(float a);
 float divide(float a);
 float area_circle(float radius);
+float area_sq(float side);
 float area_rec(float len, float width);
 float area_tri(float height, float base);
+float root(float intput);
 
+typedef struct 
+{
+    char *question;
+    char *options[4];
+    int correct_option;
+
+} Question;
+
+Question questions[] = {
+    {"What is the capital of France?", {"A. Paris", "B. Rome", "C. London", "D. Berlin"}, 0},
+    {"Who wrote 'Romeo and Juliet'?", {"A. William Shakespeare", "B. Charles Dickens", "C. Jane Austen", "D. Mark Twain"}, 0},
+    {"What is the chemical symbol for water?", {"A. H2O", "B. CO2", "C. O2", "D. NaCl"}, 0},
+    {"What is the tallest mammal?", {"A. Giraffe", "B. Elephant", "C. Hippopotamus", "D. Kangaroo"}, 0},
+    {"What is the largest planet in our solar system?", {"A. Jupiter", "B. Saturn", "C. Mars", "D. Earth"}, 0}
+};
+
+// function to suffele the option
+void suffle_option(char **option){
+    srand(time(NULL));
+    for(int i = 4; i>0; i--){
+        int j = rand() % (i+1);
+        // swaping option:
+        char *temp = option[i];
+        option[i] = option[j];
+        option[j] = temp;
+    }
+}
+
+// function to suffle questions and options:
+
+void suffle_question(Question *question, int num_question){
+    srand(time(NULL));
+    for(int i = num_question - 1; i>0; i--){ // using num_quseiton -1 because in c the number starts from 0 not 1
+        int j= rand() % (i+1);
+        // swaping the question:
+        Question temp = question[i];
+        question[i] = question[j];
+        question[j] = temp;
+
+    }
+    // suffling option for each question:
+    for (int i = 0; i < num_question; i++){
+        suffle_option(question[i].options); // this will keep track of the qusetion and the respective answer when swaping both
+
+    }
+    
+    
+}
+
+void display_option(char **options){
+    for(int i = 0; i<4; i++){
+        printf("%s\n", options[i]);
+    }
+}
+
+// writing fucntion to select random question:
+Question select_question(){
+    return questions[rand()% (sizeof(questions) / sizeof(questions[0]))];
+}
+
+// function to check if the answer is correct:
+
+int check_answer(int selected_option, int correct_option){
+    return selected_option == correct_option;
+}
+
+void play_game(){
+    printf("Welcome to Kaun Banega Crorepati!\n");
+    printf("You will be asked a series of questions. Choose the correct option to win!\n");
+    int num_questions = sizeof(questions) / sizeof(questions[0]);
+    suffle_question(questions, num_questions);
+    // writing for loop to display question and options from the array: 
+    for(int i = 0; i<num_questions; i++){
+        printf("Question number %d: %s", i+1, questions[i].question); // this will collect the question from the question array till the time i is less than number of questoin
+        // after this we will display option with respective to the question.
+        display_option(questions[i].options);
+
+        // trying to fetech the correct option for the respective question:
+        char *original_correct_option = questions[i].options[questions[i].correct_option];
+        // int suffled_correct_option = find_correct_option[]
+        int find_correct_option;
+    int shuffled_correct_option = find_correct_option[questions[i].options, questions[i].correct_option, original_correct_option];
+
+        printf("Enter 0 to 3 repectively for the correct option of respective quetions\n");
+        int answer;
+        printf("Enter your option: ");
+        scanf("%d", &answer);
+
+        if (answer == -1){
+            printf("It was fun playing with you Thank you");
+            return;
+        }
+
+        if (check_answer(answer, shuffled_correct_option)){
+            printf("Yes this is a correct option congratulation: ");
+        }
+        else{
+            printf("Sorry the answer was incorrect \n\n");
+            printf("The correct option was %s", original_correct_option);
+
+        }
+    }
+    printf("congo you completed the game 🎉");
+
+}
 
 int main(){
     
@@ -56,7 +166,13 @@ int main(){
         float height, base;
         area_tri(height, base);
     }
-    
+    else if(strcmp(user_input, "area_sq") == 0|| strcmp(user_input, "area_of_sq") == 0){
+        float side;
+        area_sq(side);
+    }    
+    else if(strcmp(user_input, "quiz") == 0){
+        play_game();
+    }
     
     return 0;
 }
@@ -141,4 +257,17 @@ float area_tri(float height, float base){
     float ans7 = 0.5*height*base;
     printf("area of triangle = %f", ans7);
 
+}
+
+float root(float input){
+    float ans8 = pow(input, 0.5);
+    printf("root of %f is %f", input, ans8);
+}
+
+float area_sq(float side){
+    printf("Please enter the side of the sq: ");
+    scanf("%f", &side);
+    float ans9 = pow(side,2);
+    // printf("area of sq = %f" ans9);
+    printf("area of sq = %f", ans9);
 }
